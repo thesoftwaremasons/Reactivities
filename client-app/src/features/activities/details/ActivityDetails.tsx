fd17670d01ccc4812c00b007d5f3909b1483f195
Babafemi Samuel Ogundipe <smiles2k6@hotmail.com>
2020-11-15 22:44:00 +0100

    end of section 5

---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Card, Image, Button } from 'semantic-ui-react'
+import { IActivity } from '../../../app/modules/activity'
+interface IProps {
+  activity: IActivity
+  setEditMode: (editMode: boolean) => void;
+  setSelectedActivity: (activity: IActivity | null) => void
+}
+
+export const ActivityDetails: React.FC<IProps> = ({ activity, setEditMode, setSelectedActivity }) => {
+  return (
+    <Card fluid>
+
+      <Image src={`/assets/categoryImages/${activity.category}.jpg`} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{activity.title}</Card.Header>
+        <Card.Meta>
+          <span className='date'>{activity.date}</span>
+        </Card.Meta>
+        <Card.Description>
+          {activity.description}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button.Group widths={2}>
+          <Button onClick={() => setEditMode(true)} basic color="blue" content="Edit"></Button>
+          <Button basic color="red" content="Cancel" onClick={() => setSelectedActivity(null)}></Button>
+        </Button.Group>
+      </Card.Content>
+    </Card>
+  )
+}
